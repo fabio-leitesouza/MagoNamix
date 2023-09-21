234784cab449303f90c441c0372ac30b1ab396f1
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2023-09-21 15:33:54 -0300

    Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,52 +7,63 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Animator animator;
-    public GameObject Staff;
+    private Vector2 currentDirection; // Armazena a direção atual
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        currentDirection = Vector2.down; // Inicialmente apontando para baixo
     }
 
     void Update()
-{
-    float moveX = Input.GetAxis("Horizontal");
-    float moveY = Input.GetAxis("Vertical");
-
-    Vector2 movement = new Vector2(moveX, moveY).normalized;
-    
-    rb.velocity = movement * moveSpeed;
-
-    if (movement.magnitude > 0.1f)
     {
-        if (moveX > 0) // Mover para a direita
+        // Obtenha a posição do mouse no mundo
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0; // Certifique-se de que a coordenada Z seja 0
+
+        // Calcule a direção do mouse em relação ao jogador
+        Vector2 direction = (mousePosition - transform.position).normalized;
+
+        // Mova o jogador com base na entrada do teclado
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveX, moveY).normalized;
+        rb.velocity = movement * moveSpeed;
+
+        // Atualize a animação apenas quando a direção mudar
+        if (direction.magnitude > 0.1f && direction != currentDirection)
         {
-            animator.SetTrigger("WalkRight"); 
+            currentDirection = direction;
+            UpdateAnimation(direction);
         }
-        else if (moveX < 0) // Mover para a esquerda
+        else if (movement.magnitude < 0.1f && direction == currentDirection)
         {
-            animator.SetTrigger("WalkLeft");
+            // Se o jogador não estiver se movendo e a direção não mudou, defina a animação para o estado idle
+            animator.SetTrigger("Idle");
         }
-        else if (moveY > 0) // Mover para cima
+    }
+
+    // Atualize a animação com base na direção atual
+    private void UpdateAnimation(Vector2 direction)
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        if (angle > -45 && angle <= 45) // Apontar para a direita
+        {
+            animator.SetTrigger("WalkRight");
+        }
+        else if (angle > 45 && angle <= 135) // Apontar para cima
         {
             animator.SetTrigger("WalkUp");
         }
-        else if (moveY < 0) // Mover para baixo
+        else if (angle > 135 || angle <= -135) // Apontar para a esquerda
+        {
+            animator.SetTrigger("WalkLeft");
+        }
+        else // Apontar para baixo
         {
             animator.SetTrigger("WalkDown");
         }
     }
-    else // Jogador parado
-    {
-        animator.ResetTrigger("WalkUp");
-        animator.ResetTrigger("WalkDown");
-        animator.ResetTrigger("WalkLeft");
-        animator.ResetTrigger("WalkRight");
-    }
-
-    animator.SetFloat("Speed", movement.magnitude);
-}
-
-
 }
